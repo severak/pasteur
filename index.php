f97b53cd333f754eee285a7499bdf38ce8ab7b7f
Severak <strajt9@seznam.cz>
2016-09-15 23:41:40 +0200

    rozfachan autoload

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 require 'lib/flight/Flight.php';
+require "lib/flight/autoload.php";
+
+flight\core\Loader::addDirectory("lib/flourish");
+
+fCore::enableDebugging(TRUE);
+
+use Nibble\NibbleForms\NibbleForm as form;
+
 
 Flight::route('/', function(){
+  $form = form::getInstance('/');
+  fCore::expose($form);
   echo 'hello world!';
 });
 
